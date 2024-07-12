6884116211c311e58a18fe732ce7df359269323c
Michael Lore <michael.lore@gmail.com>
2024-07-12 16:33:00 -0400

    Clean up execv

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"rsc.io/script"
 )
@@ -21,35 +22,76 @@ func main() {
 	flag.Parse()
 
 	for _, scr := range flag.Args() {
-		engine := script.NewEngine()
-		// engine.ListCmds(os.Stdout, true)
-		// engine.ListConds(os.Stdout, nil)
-		state, err := script.NewState(context.Background(), ".", os.Environ())
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-		f, err := os.Open(scr)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-		defer f.Close()
-		reader := bufio.NewReader(f)
-		var logf bytes.Buffer
-		err = engine.Execute(state, scr, reader, &logf)
+		stdout, stderr, scrlog, err := runScript(scr)
+
 		if *printLog {
-			fmt.Println(logf.String())
+			fmt.Println(scrlog)
 		}
 		if *printOut {
-			fmt.Println(state.Stdout())
+			fmt.Println(stdout)
 		}
 		if *printErr {
-			fmt.Fprintln(os.Stderr, state.Stderr())
+			fmt.Fprintln(os.Stderr, stderr)
 		}
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			return
+			os.Exit(1)
 		}
 	}
+}
+
+func runScript(scr string) (stdout string, stderr string, scrlog string, err error) {
+	engine := script.NewEngine()
+	// engine.ListCmds(os.Stdout, true)
+	// engine.ListConds(os.Stdout, nil)
+	engine.Cmds["execv"] = execv(engine.Cmds["exec"])
+	var state *script.State
+	state, err = script.NewState(context.Background(), ".", os.Environ())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	f, err := os.Open(scr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer f.Close()
+	reader := bufio.NewReader(f)
+	var logf bytes.Buffer
+	err = engine.Execute(state, scr, reader, &logf)
+	scrlog = logf.String()
+	stdout = state.Stdout()
+	stderr = state.Stderr()
+	return
+}
+
+func execv(execCmd script.Cmd) script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "run an executable program with arguments",
+			Args:    "program [args...]",
+			Detail: []string{
+				"Note that 'exec' does not terminate the script (unlike Unix shells).",
+				"Arguments will be additionally expanded for command options.",
+			},
+			Async: true,
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) < 1 {
+				return nil, script.ErrUsage
+			}
+			var newArgs []string
+			for _, arg := range args {
+				list := strings.Split(strings.ReplaceAll(arg, "\t", " "), " ")
+				for _, s := range list {
+					s = strings.TrimSpace(s)
+					if s != "" {
+						newArgs = append(newArgs, s)
+					}
+				}
+			}
+			return execCmd.Run(s, newArgs...)
+		})
 }
